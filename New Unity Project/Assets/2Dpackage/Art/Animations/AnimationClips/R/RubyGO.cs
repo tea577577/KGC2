e6f7b6618446bb9dc688a165fb2e65332979ec22
tea577577 <91519801+ATsau0620@users.noreply.github.com>
2021-11-23 16:11:08 +0800

    1123

---
+++ New Unity Project/Assets/2Dpackage/Art/Animations/AnimationClips/R/RubyGO.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyGO : MonoBehaviour
+{
+    private Vector2 lookDirection;
+    private Vector2 rubyPosition;
+    private Vector2 rubyMove;
+
+    public Animator rubyAnimator;
+    public Rigidbody2D rb;
+
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rubyAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rubyPosition = transform.position;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        print("Horizontal is:" + horizontal);
+        print("Vertical is:" + vertical);
+
+        rubyMove = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(rubyMove.x, 0) || !Mathf.Approximately(rubyMove.y, 0))
+        {
+            lookDirection = rubyMove;
+            lookDirection.Normalize();
+        }
+
+        rubyAnimator.SetFloat("Look X", lookDirection.x);
+        rubyAnimator.SetFloat("Look Y", lookDirection.y);
+        rubyAnimator.SetFloat("Speed", rubyMove.magnitude);
+
+        rubyPosition = rubyPosition + speed * rubyMove * Time.deltaTime;
+        rb.MovePosition(rubyPosition);
+    }
+}
